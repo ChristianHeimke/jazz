82c6b96c0706c0f72c2ba67f7e12a0a70d931ffd
Christian Heimke <info@heimke.net>
2012-05-16 21:16:35 +0200

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jazz::Application.config.secret_token = '2ed096e3f746436877dcdc071355ebce95a06db55ab2fb49a91a07573c4ae5896c8889d7359e4d38836a35975a48fb86799a9689be63c80c1a39d2125bc33009'
